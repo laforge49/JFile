8e2896a1416ce16b5e00b1fd88eb1e4140fa0f77
Bill LaForge <laforge49@gmail.com>
2012-05-03 17:21:44 +0530

    new Block interface

---
+++ src/main/java/org/agilewiki/jfile/block/Block.java
@@ -0,0 +1,38 @@
+package org.agilewiki.jfile.block;
+
+import org.agilewiki.jid.scalar.vlens.actor.RootJid;
+
+/**
+ * A wrapper for data to be read from or written to disk.
+ * The header added to the serialized data contains the length of the serialized
+ * data and, optionally, a timestamp and checksum.
+ */
+public interface Block {
+    /**
+     * Convert a RootJid to a byte array that is prefaced by a header.
+     * @param rootJid The RootJid to be serialized.
+     * @return A byte array containing both a header and the serialized RootJid.
+     */
+    public byte[] serialize(RootJid rootJid);
+    
+    /**
+     * The length of the header which prefaces the actual data on disk.
+     * @return The header length.
+     */
+    public int headerLength();
+
+    /**
+     * Provides the raw header information.
+     * 
+     * @param bytes The header bytes read from disk.
+     * @return The length of the data following the header on disk.
+     */
+    public int setHeader(byte[] bytes);
+
+    /**
+     * Deserialize the data following the header on disk.
+     * @param bytes The data following the header on disk.
+     * @return The deserialized RootJid.
+     */
+    public RootJid deserialize(byte[] bytes) throws Exception;
+}
