de7a63a07e4945f30768022895dfe0f9b45489cd
Bill LaForge <laforge49@gmail.com>
2012-05-07 21:03:42 +0530

    fixed a bug in NullTransaction

--- src/test/java/org/agilewiki/jfile/transactions/NullTransaction.java
+++ src/test/java/org/agilewiki/jfile/transactions/NullTransaction.java
@@ -2,15 +2,20 @@ package org.agilewiki.jfile.transactions;
 
 import org.agilewiki.jactor.Mailbox;
 import org.agilewiki.jactor.RP;
-import org.agilewiki.jactor.lpc.JLPCActor;
+import org.agilewiki.jid.Jid;
 
-public class NullTransaction extends JLPCActor {
+public class NullTransaction extends Jid {
     public NullTransaction(Mailbox mailbox) {
         super(mailbox);
     }
 
     @Override
     protected void processRequest(Object request, RP rp) throws Exception {
-        rp.processResponse(null);
+        if (request.getClass() == Eval.class) {
+            rp.processResponse(null);
+            return;
+        }
+
+        super.processRequest(request, rp);
     }
 }
