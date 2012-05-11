22201d66ce8323477622ead9b119b23745a2799c
Bill LaForge <laforge49@gmail.com>
2012-05-11 14:59:46 +0530

    first, simplistic timing test for transaction logger

--- src/test/java/org/agilewiki/jfile/transactions/transactionLogger/TransactionLoggerTimingTest.java
+++ src/test/java/org/agilewiki/jfile/transactions/transactionLogger/TransactionLoggerTimingTest.java
@@ -8,7 +8,6 @@ import org.agilewiki.jactor.MailboxFactory;
 import org.agilewiki.jactor.factory.JAFactory;
 import org.agilewiki.jfile.JFile;
 import org.agilewiki.jfile.JFileFactories;
-import org.agilewiki.jfile.transactions.HelloWorldTransaction;
 import org.agilewiki.jfile.transactions.NullTransactionFactory;
 import org.agilewiki.jfile.transactions.db.StatelessDB;
 import org.agilewiki.jfile.transactions.transactionProcessor.TransactionProcessor;
@@ -18,14 +17,15 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
-public class TransactionLoggerTest extends TestCase {
+public class TransactionLoggerTimingTest extends TestCase {
     public void test()
             throws Exception {
         MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(10);
         Mailbox factoryMailbox = mailboxFactory.createMailbox();
         JAFactory factory = new JAFactory(factoryMailbox);
         (new JFileFactories(factoryMailbox)).setParent(factory);
-        factory.defineActorType("helloWorldTransaction", HelloWorldTransaction.class);
+        NullTransactionFactory ntf = new NullTransactionFactory("n");
+        factory.registerActorFactory(ntf);
         JAFuture future = new JAFuture();
         Mailbox dbMailbox = mailboxFactory.createAsyncMailbox();
         StatelessDB db = new StatelessDB(dbMailbox);
@@ -35,7 +35,7 @@ public class TransactionLoggerTest extends TestCase {
 
         JFile jFile = new JFile(mailboxFactory.createAsyncMailbox());
         jFile.setParent(transactionProcessor);
-        Path path = FileSystems.getDefault().getPath("TransactionLoggerTest.jf");
+        Path path = FileSystems.getDefault().getPath("TransactionLoggerTimingTest.jf");
         System.out.println(path.toAbsolutePath());
         jFile.fileChannel = FileChannel.open(
                 path,
@@ -47,9 +47,16 @@ public class TransactionLoggerTest extends TestCase {
                 new TransactionLogger(mailboxFactory.createAsyncMailbox());
         transactionLogger.setParent(jFile);
 
-        (new ProcessTransaction("helloWorldTransaction")).sendEvent(transactionLogger);
-        (new ProcessTransaction("helloWorldTransaction")).sendEvent(transactionLogger);
-        (new ProcessTransaction("helloWorldTransaction")).send(future, transactionLogger);
+        long t0 = System.currentTimeMillis();
+        int i = 0;
+        while (i < 99) {
+            (new ProcessTransaction(ntf)).sendEvent(transactionLogger);
+            i += 1;
+        }
+        (new ProcessTransaction(ntf)).send(future, transactionLogger);
+        long t1 = System.currentTimeMillis();
+
+        System.out.println("milliseconds: " + (t1 - t0));
 
         jFile.fileChannel.close();
         mailboxFactory.close();
