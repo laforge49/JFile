36083a106f1e09a411339fc5da666501c098b095
Bill LaForge <laforge49@gmail.com>
2012-05-14 08:56:55 +0530

    TransactionAggregatorTest is working

--- src/main/java/org/agilewiki/jfile/transactions/transactionLogger/TransactionAggregator.java
+++ src/main/java/org/agilewiki/jfile/transactions/transactionLogger/TransactionAggregator.java
@@ -1,3 +1,26 @@
+/*
+ * Copyright 2012 Bill La Forge
+ *
+ * This file is part of AgileWiki and is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License (LGPL) as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * or navigate to the following url http://www.gnu.org/licenses/lgpl-2.1.txt
+ *
+ * Note however that only Scala, Java and JavaScript files are being covered by LGPL.
+ * All other files are covered by the Common Public License (CPL).
+ * A copy of this license is also included and can be
+ * found as well at http://www.opensource.org/licenses/cpl1.0.txt
+ */
 package org.agilewiki.jfile.transactions.transactionLogger;
 
 import org.agilewiki.jactor.Mailbox;
@@ -7,7 +30,6 @@ import org.agilewiki.jactor.factory.JAFactoryFactory;
 import org.agilewiki.jactor.factory.NewActor;
 import org.agilewiki.jactor.factory.Requirement;
 import org.agilewiki.jactor.lpc.JLPCActor;
-import org.agilewiki.jfile.ForcedWriteRootJid;
 import org.agilewiki.jfile.JFileFactories;
 import org.agilewiki.jfile.block.Block;
 import org.agilewiki.jfile.block.LTA32Block;
@@ -113,16 +135,20 @@ public class TransactionAggregator extends JLPCActor implements _TransactionAggr
 
     private void writeBlock()
             throws Exception {
+        if (rootJid == null)
+            return;
         final Block block = newBlock();
         block.setRootJid(rootJid);
         rootJid = null;
         block.setTimestamp(newTimestamp());
         writePending = true;
-        (new ForcedWriteRootJid(block)).send(this, next, new RP<Object>() {
+        (new ProcessBlock(block)).send(this, next, new RP<Object>() {
             @Override
             public void processResponse(Object response)
                     throws Exception {
                 writePending = false;
+                if (getMailbox().isEmpty())
+                    writeBlock();
             }
         });
         getMailbox().sendPendingMessages();
---
+++ src/test/java/org/agilewiki/jfile/transactions/transactionLogger/TransactionAggregatorTest.java
@@ -0,0 +1,40 @@
+package org.agilewiki.jfile.transactions.transactionLogger;
+
+import junit.framework.TestCase;
+import org.agilewiki.jactor.JAFuture;
+import org.agilewiki.jactor.JAMailboxFactory;
+import org.agilewiki.jactor.Mailbox;
+import org.agilewiki.jactor.MailboxFactory;
+import org.agilewiki.jactor.factory.JAFactory;
+import org.agilewiki.jfile.JFileFactories;
+import org.agilewiki.jfile.transactions.HelloWorldTransaction;
+import org.agilewiki.jfile.transactions.TransactionProcessor;
+import org.agilewiki.jfile.transactions.db.StatelessDB;
+
+public class TransactionAggregatorTest extends TestCase {
+    public void test()
+            throws Exception {
+        MailboxFactory mailboxFactory = JAMailboxFactory.newMailboxFactory(10);
+        Mailbox factoryMailbox = mailboxFactory.createMailbox();
+        JAFactory factory = new JAFactory(factoryMailbox);
+        (new JFileFactories(factoryMailbox)).setParent(factory);
+        factory.defineActorType("helloWorldTransaction", HelloWorldTransaction.class);
+        JAFuture future = new JAFuture();
+        Mailbox dbMailbox = mailboxFactory.createAsyncMailbox();
+        StatelessDB db = new StatelessDB(dbMailbox);
+        db.setParent(factory);
+        TransactionProcessor transactionProcessor = new TransactionProcessor(dbMailbox);
+        transactionProcessor.setParent(db);
+
+        TransactionAggregator transactionAggregator =
+                new TransactionAggregator(mailboxFactory.createAsyncMailbox());
+        transactionAggregator.setParent(factory);
+        transactionAggregator.next = transactionProcessor;
+
+        (new AggregateTransaction("helloWorldTransaction")).sendEvent(transactionAggregator);
+        (new AggregateTransaction("helloWorldTransaction")).sendEvent(transactionAggregator);
+        (new AggregateTransaction("helloWorldTransaction")).send(future, transactionAggregator);
+
+        mailboxFactory.close();
+    }
+}
